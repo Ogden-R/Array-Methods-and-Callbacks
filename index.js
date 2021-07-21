a94b38fbbda8a256d902693f6986387df1087f7e
Ogden-R <rebeccaogden97@gmail.com>
2021-07-20 21:45:13 -0500

    finished all MVP tasks and all tests are passing on them

--- index.js
+++ index.js
@@ -89,16 +89,17 @@ Use the higher order function getAverageGoals to do the following:
  (Hint: use .reduce and do this in 2 steps) 
  //look up .toFixed()
 
-
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
 
 
-function getAverageGoals(/* code here */) {
-   /* code here */
+ function getAverageGoals(data) {
+   const averageGoals = data.reduce((acc, item) => acc + item['Home Team Goals'] + item['Away Team Goals']
+ , 0 )
+ return (averageGoals / data.length).toFixed(2)
 }
-
+console.log("Task 6:", getAverageGoals(getFinals(fifaData)))
 
 
 
